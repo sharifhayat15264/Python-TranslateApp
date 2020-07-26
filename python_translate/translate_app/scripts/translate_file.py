f0d628574eb2a510612c98951ae81640d51d204f
sharifhayat15264 <58738675+sharifhayat15264@users.noreply.github.com>
2020-07-27 00:42:20 +0500

    Added support for translating long text over 15K characters by using NLTK.

--- python_translate/translate_app/scripts/translate_file.py
+++ python_translate/translate_app/scripts/translate_file.py
@@ -1,4 +1,5 @@
 import PyPDF2
+import nltk.data
 from docx import Document
 from googletrans import Translator, LANGUAGES
 
@@ -10,19 +11,27 @@ def translate_file(filename, dest_lang, src_lang):
     src_lang = src_lang.lower()
 
     dest_code = langcodes[dest_lang]
-
     src_code = langcodes[src_lang]
 
     try:
-        with open(filename, "r") as txt_file:
+        with open(filename, "r", encoding="utf8") as txt_file:
             text = txt_file.read()
-            translator = Translator()
-            translated = translator.translate(text, dest_code, src_code)
-            translation = translated.text
-            print(translation)
-            with open("result.txt", "w") as result:
-                result.write(translation)
-                print("File Created!")
+
+            # if text has 15K or more characters:
+            if len(text) >= 15000:
+                complete_translation = translate_long_text(text, dest_code, src_code)
+                with open("result.txt", "w") as result:
+                    result.write(complete_translation)
+                    print("File Created!")
+                    return
+            else:
+                translator = Translator()
+                translated = translator.translate(text, dest_code, src_code)
+                translation = translated.text
+                print(translation)
+                with open("result.txt", "w") as result:
+                    result.write(translation)
+                    print("File Created!")
 
     except FileNotFoundError:
         print("File not found")
@@ -40,20 +49,29 @@ def translate_pdf(filename, dest_lang, src_lang):
     try:
         with open(filename, "rb") as pdf_file:
             reader = PyPDF2.PdfFileReader(pdf_file)
-            # print(reader.numPages)
-            page = reader.getPage(0)
-            text = page.extractText()
+            pages = reader.numPages
+            text = ""
+            for i in range(pages):
+                page = reader.getPage(i)
+                text += page.extractText()
             full_text = text.replace("\n", "")
-            # print(f, type(text))
 
-            translator = Translator()
-            translated = translator.translate(full_text, dest_code, src_code)
-            translation = translated.text
-            print(translation)
-            with open("result_pdf.txt", "w") as result:
-                result.write(translation)
-                print("File Created!")
-                return
+            if len(full_text) >= 15000:
+                complete_translation = translate_long_text(full_text, dest_code, src_code)
+                with open("result_pdf.txt", "w") as result:
+                    result.write(complete_translation)
+                    print("File Created!")
+                    return
+            else:
+                translator = Translator()
+                translated = translator.translate(full_text, dest_code, src_code)
+                translation = translated.text
+                print(translation)
+                with open("result_pdf.txt", "w") as result:
+                    result.write(translation)
+                    print("File Created!")
+                    return
+
     except FileNotFoundError:
         print("File not found.")
         return
@@ -62,7 +80,7 @@ def translate_pdf(filename, dest_lang, src_lang):
 
 
 # DOCS #
-def get_text_docs(filename):
+def get_text_docs(filename, dest_lang, src_lang):
     doc = Document(filename)
     full_text = list()
     for para in doc.paragraphs:
@@ -80,10 +98,61 @@ def translate_docs(filename, dest_lang, src_lang):
     src_code = langcodes[src_lang]
 
     text = get_text_docs(filename)
+
+    if len(text) >= 15000:
+        complete_translation = translate_long_text(text, dest_code, src_code)
+        return complete_translation
+    else:
+        translator = Translator()
+        translated = translator.translate(text, dest_code, src_code)
+        translation = translated.text
+        return translation
+
+
+# call if text is very long
+def translate_long_text(text, dest_code, src_code):
+    # nltk code goes here bro
+    valid_lang = {"cs" : "czech", "da" : "danish", "nl" : "dutch", "en" : "english", "et" : "estonian",
+                         "fi" : "finnish", "fr" : "french", "de" : "german", "el" : "greek", "it" : "italian",
+                         "no" : "norwegian", "pl" : "polish", "pt" : "portuguese", "ru" : "russian", "sl" : "slovene",
+                         "es" : "spanish", "sv" : "swedish", "tr" : "turkish"}
+
+    if src_code in valid_lang:
+        src_language = valid_lang[src_code]
+        src_tokenizer = nltk.data.load(f"tokenizers\punkt\{src_language}.pickle")
+        text_list = src_tokenizer.tokenize(text)
+        with open("tokens.txt", "w") as token:
+            token.write(str(text_list))
+    else:
+        print("Not a recognized source language for long translation")
+        return 1
+
     translator = Translator()
-    translated = translator.translate(text, dest_code, src_code)
+    translation_list = []
+
+    temp_str = ""
+    total_len = 0
+    for t in text_list:
+        current_len = len(temp_str)
+        total_len += len(t)
+        print(current_len)
+        if (current_len + len(t)) > 10000:
+            print("translating...")
+            translated = translator.translate(temp_str, dest_code, src_code)
+            translation = translated.text
+            translation_list.append(translation)
+            temp_str = ""
+        else:
+            temp_str += " " + t
+
+    translated = translator.translate(temp_str, dest_code, src_code)
     translation = translated.text
-    return translation
+    translation_list.append(translation)
+
+    print(len(translation_list))
+
+    complete_translation = "".join(translation_list)
+    return complete_translation
 
 
 def generate_file(translation):
@@ -96,4 +165,26 @@ def driver(filename, dest_lang, src_lang):
     generate_file(translation)
 
 
-print(langcodes)
+
+
+
+
+
+
+
+
+
+
+
+
+# print(langcodes)
+# with open("sample.txt", "r", encoding="utf8") as file:
+#     text = file.read()
+#     len_original = len(text)
+#     result = translate_long_text(text, "fr", "en")
+#     with open("result.txt", "w") as nigger:
+#         nigger.write(result)
+
+# with open("str_t.txt", "r") as file:
+#     text = file.read()
+#     translate_long_text(text, "fr", "en")
--- python_translate/translate_app/scripts/translate_text.py
+++ python_translate/translate_app/scripts/translate_text.py
@@ -14,3 +14,22 @@ def translate_text(text, dest_lang, src_lang):
     translated = translator.translate(str(text), dest_code, src_code)
     translation = translated.text
     return translation
+
+def translate_long_text(text, dest_lang, src_lang):
+    n = 14900
+    text_list = [(text[i:i + n]) for i in range(0, len(text), n)]
+    translation_list = list()
+    translator = Translator()
+    for t in text_list:
+        translated = translator.translate(str(t), dest_code, src_code)
+        translation = translated.text
+        translation_list.append(translation)
+
+    complete_translation = "".join(translation_list)
+    return complete_translation
+
+
+
+sample = "These excellant intentions were strengthed when he enterd the Father Superior's diniing-room, though, stricttly speakin, it was not a dining-room, for the Father Superior had only two rooms alltogether; they were, however, much larger and more comfortable than Father Zossima's. But tehre was was no great luxury about the furnishng of these rooms eithar. The furniture was of mohogany, covered with leather, in the old-fashionned style of 1820 the floor was not even stained, but evreything was shining with cleanlyness, and there were many chioce flowers in the windows; the most sumptuous thing in the room at the moment was, of course, the beatifuly decorated table. The cloth was clean, the service shone; there were three kinds of well-baked bread, two bottles of wine, two of excellent mead, and a large glass jug of kvas -- both the latter made in the monastery, and famous in the neigborhood. There was no vodka. Rakitin related afterwards that there were five dishes: fish-suop made of sterlets, served with little fish paties; then boiled fish served in a spesial way; then salmon cutlets, ice pudding and compote, and finally, blanc-mange. Rakitin found out about all these good things, for he could not resist peeping into the kitchen, where he already had a footing. He had a footting everywhere, and got informaiton about everything. He was of an uneasy and envious temper. He was well aware of his own considerable abilities, and nervously exaggerated them in his self-conceit. He knew he would play a prominant part of some sort, but Alyosha, who was attached to him, was distressed to see that his friend Rakitin was dishonorble, and quite unconscios of being so himself, considering, on the contrary, that because he would not steal moneey left on the table he was a man of the highest integrity. Neither Alyosha nor anyone else could have infleunced him in that."
+# a = translate_text(sample, "French", "English")
+print(len(sample))
